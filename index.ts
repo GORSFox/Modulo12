ca2ba0712902c48a4ef013230d94d5e9123bafbe
Glaucia Rapozo <97680820+GORSFox@users.noreply.github.com>
2022-09-20 19:26:37 -0300

    Inserindo arquivos

--- index.ts
+++ index.ts
@@ -1,47 +1,49 @@
-/*
+// Boolean 
 
-let isSet: boolean= false;
+let isSet: boolean = false;
 isSet = true;
 
+// Number
 let decimal: number = 6;
-let hex: number = 0xff0000; // colocar o 0x para indicar hexadec
-let binary: number = 0b010101;//colocar o 0b para binary
+let hex: number = 0xff0000;
+let binary: number = 0b01010;
 
-let color: string = 'blue';
+// String
+let color: string = 'blue'
 color = 'red';
 
-let list: number[] = [1,2,3]
-let list2: Array<number> = [1,2,3]
+// Array 
+let list: number[] = [1, 2, 3]
+let list2: Array<number> = [1, 2, 3]
 
+// Tuple 
 let user: [string, number];
-user = ["Glaucia", 1]
+user = ["Marcelo", 1]
 
-let State = 'inactive' | 'active' |'destack' //estados
-
-//Alias - atalho- usuário específico/ passar como tipo
+// Alias
 type User = {
     name: string;
     age: number;
     id: number;
-    city: string;
-
+    city: string
 }
 
-let user1: User;
+let user1 : User;
 
-type pet = 'dog' | 'cat';
+type pet = 'dog' | 'cat' | 'fish';
 
 let pet: pet = 'dog';
-let pet2: pet = 'cat'*/
+let pet2: pet = 'fish';
 
-function sum(n1:number | string, n2:number | string){
+function sum(n1: number | string, n2: number | string){
 
-    let v1 = typeof n1 ==='string'? +n1:n1;
-    let v2 = typeof n2 ==='string'? +n2:n2;
+    let v1 = typeof n1 === 'string' ? +n1 : n1;
+    let v2 = typeof n2 === 'string' ? +n2 : n2;
 
     console.log(v1 + v2);
+    
 }
 
-sum(20,'20');
-sum(50,"50");
-sum("10","10");
+sum(20, '20'); //ok
+sum(50, "50"); //ok
+sum("10", "10"); // ok
